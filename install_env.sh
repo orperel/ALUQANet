2ec5aeefc22430fbc282e10616c6a5936597ed05
Or Perel <orperel@amazon.com>
2019-07-20 18:48:52 +0300

    updated install_env.sh to accomodate tensorboard support

--- install_env.sh
+++ install_env.sh
@@ -1,4 +1,6 @@
 conda create -n aluqa python=3.6
 source activate aluqa
 conda install pytorch torchvision cudatoolkit=10.0 -c pytorch
+pip install tb-nightly
+pip install future
 pip install allennlp
